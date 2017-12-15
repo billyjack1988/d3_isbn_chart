a749594e5fdcd1a19208ff881e4e91a2027e520b
billyjack1988 <billyjacktattoos@gmail.com>
2017-12-15 15:17:09 -0500

    pushing for luke

--- app.rb
+++ app.rb
@@ -7,31 +7,18 @@ require 'aws-sdk'
 load './local_env.rb' if File.exist?('./local_env.rb')
 enable :sessions
 
-get '/' do
-    list = get_file()
-    # p "#{list}"
-    erb :raw_data, locals: {list:list}
-end    
+# get '/' do
+#     list = get_file()
+#     # p "#{list}"
+#     erb :raw_data, locals: {list:list}
+# end    
 
-get '/d3_protype/employees.csv' do    
-    arr_of_arrs = CSV.read("employee.csv")
-    p "#{arr_of_arrs}"
+get '/' do    
+     x = CSV.read("employees.csv").flatten
+        p "#{x}"
+    erb :first_daft, locals: {x:x}
+     
 end
 
-get '/d3_isbn_chart/employees.csv' do
-    arr_of_arrs = CSV.read("employees.csv")
-    
-    p  "#{arr_of_arrs}"
-    erb :raw_data, locals: {arr_of_arrs:arr_of_arrs}
-    
-    
-    
-    # x = []
-    # CSV.open("employees.csv", "wb") do |csv|
-    #     CSV.parse("employees.csv,data,String") do |row|
-    #         x << row
-    #     end    
-    # end    
-    # p  "#{x}"
-end    
+  
 
