5066e4d7a70ec71ca1b87b674dafbebee6116eb3
Ldeems <luke.deems@yahoo.com>
2017-12-18 10:11:21 -0500

    got the functions made to separate and count the list of isbn numbers

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative 'bucket_func.rb'
+require_relative 'functions.rb'
 require "csv"
 require 'rubygems'
 require 'aws-sdk'
@@ -7,49 +8,22 @@ require 'aws-sdk'
 load './local_env.rb' if File.exist?('./local_env.rb')
 enable :sessions
 
-# get '/' do
-#     list = get_file()
-#     # p "#{list}"
-#     erb :raw_data, locals: {list:list}
-# end    
+get '/' do
+    list = get_file()
+    validated = gettingvalid(list)
+    validcount = howmanyvalid(validated)
+    invalidcount = howmanyinvalid(validated)
+    infoarr = joinscounts(validcount,invalidcount)
+     p "#{infoarr}"
+    #erb :raw_data, locals: {list:list}
+end    
 
-get '/' do    
-     x = CSV.read("employees.csv").flatten
-        p "#{x}"
-    erb :first_daft, locals: {x:x}
+# get '/' do    
+#      x = CSV.read("employees.csv")
+#         p "#{x}"
+#     erb :first_daft, locals: {x:x}
      
-end
+# end
 
-<<<<<<< HEAD
-# get '/d3_isbn_chart/employees.csv' do
-#     a = []
-#     x = CSV.read("employees.csv")
-#     CSV.parse(x) do |row|
-#         a << row
-#     end
-#     "#{a}"
-    
-    
-    # arr_of_arrs = CSV.read("employees.csv")
-    # a = 
-    # p  "#{arr_of_arrs}"
-    # arr_of_arrs
-    # x = ["returnthis"]
-    # x = arr_of_arrs.to_a.flatten
-    # p  "#{x}"
-    # "#{x}"
-    # erb :raw_data, locals: {x:x}
-    
-    
-    # x = []
-    # CSV.open("employees.csv", "wb") do |csv|
-    #     CSV.parse("employees.csv,data,String") do |row|
-    #         x << row
-    #     end    
-    # end    
-    # p  "#{x}"
-#end    
-=======
   
->>>>>>> a749594e5fdcd1a19208ff881e4e91a2027e520b
 
