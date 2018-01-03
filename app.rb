48ad71dd6a6f4ee9272959b90e9ca60697a56a9f
Ldeems <luke.deems@yahoo.com>
2018-01-03 10:50:35 -0500

    notting ang re labling some routing to make it easier to navigate

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require 'pg'
 load './local_env.rb' if File.exist?('./local_env.rb')
 enable :sessions
 
+#goin to page one
 get '/' do
     check_connection()
     info = getinfo()
@@ -21,29 +22,37 @@ get '/' do
      erb :first_daft, locals: {}
 end    
 
+#to second page with animated bar graph
 post '/second_darft' do
     erb :second_darft
   end
-
-  post '/offthewall' do
-    erb :raw_data 
-  end
-
-  post '/yup' do
-    erb :try_it
-  end
-
+  
+#to usa map with all the counties  
   post '/usamap' do
     erb :usa_map
   end
 
-  post '/ld_page' do
-    erb :ld_page
+#to the page that have the coffee info  
+  post '/coffee' do 
+    erb :coffee
+  end 
+
+#to the third page with the simple line graph  
+  post '/offthewall' do
+    erb :raw_data 
+  end
+
+
+#to the fourth page with a multi line chart  
+  post '/yup' do
+    erb :fourth_page
+  end
+
+#to the fith page with a sidways bar chart the has tooltips
+  post '/to_fith_page' do
+    erb :fith_page
 end
 
-post '/coffee' do 
-  erb :coffee
-end 
 
 
 
