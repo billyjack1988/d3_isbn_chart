a10971f094a5c4ad1a20c35eb7eeb4781e8bd72c
Ldeems <luke.deems@yahoo.com>
2018-01-03 10:57:47 -0500

    fixed more routing tand notting to clean it up some

--- app.rb
+++ app.rb
@@ -19,12 +19,12 @@ get '/' do
     # invalidcount = howmanyinvalid(validated)
     # infoarr = joinscounts(validcount,invalidcount)
     #p "#{info}"
-     erb :first_daft, locals: {}
+     erb :first_page, locals: {}
 end    
 
 #to second page with animated bar graph
-post '/second_darft' do
-    erb :second_darft
+post '/to_second_page' do
+    erb :second_page
   end
   
 #to usa map with all the counties  
@@ -38,13 +38,13 @@ post '/second_darft' do
   end 
 
 #to the third page with the simple line graph  
-  post '/offthewall' do
-    erb :raw_data 
+  post '/to_third_page' do
+    erb :third_page
   end
 
 
 #to the fourth page with a multi line chart  
-  post '/yup' do
+  post '/to_fourth_page' do
     erb :fourth_page
   end
 
